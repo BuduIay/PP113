96494cada1fe15059a25b7809fcdf528270f1a34
BuduIay <ltirionl@mail.ru>
2025-02-23 23:58:07 +0300

    fff

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/555";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Root";
+
+    public static Connection connect() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Соединение с БД успешно установлено!");
+        } catch (SQLException e) {
+            System.out.println("Ошибка при подключении к БД: ");
+        }
+        return connection;
+    }
 }
+
